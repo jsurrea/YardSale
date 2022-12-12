b1646da7a3e0d70786cfe6f699351f98c0718483
Juan Sebastian Urrea Lopez <jurrea49@hotmail.com>
2022-12-12 02:52:24 -0500

    Dynamic product list

--- main.js
+++ main.js
@@ -37,3 +37,50 @@ function toggleProductAside() {
         desktopMenu.classList.add('inactive')
     }
 }
+
+const productList = []
+for (let i = 0; i < 12; i++) {
+    productList.push({
+        name: 'Bike',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    })
+}
+
+function renderProducts(productList) {
+    for(product of productList) {
+        const cardsContainer = document.querySelector('.cards-container')
+    
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+        cardsContainer.appendChild(productCard)
+    
+        const img = document.createElement('img')
+        img.setAttribute('src', product.image)
+        productCard.appendChild(img)
+    
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+        productCard.appendChild(productInfo)
+        
+        const productInfoDiv = document.createElement('div')
+        productInfo.appendChild(productInfoDiv)
+        
+        const productPrice = document.createElement('p')
+        productPrice.innerText = '$' + product.price
+        productInfoDiv.appendChild(productPrice)
+        
+        const productName = document.createElement('p')
+        productName.innerText = product.name
+        productInfoDiv.appendChild(productName)
+        
+        const productInfoFigure = document.createElement('figure')
+        productInfo.appendChild(productInfoFigure)
+    
+        const productFigureImg = document.createElement('img')
+        productFigureImg.setAttribute('src', 'icons/bt_add_to_cart.svg')
+        productInfoFigure.appendChild(productFigureImg)
+    }
+}
+
+renderProducts(productList)
